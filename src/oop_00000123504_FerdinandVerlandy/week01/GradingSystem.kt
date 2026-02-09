5487a8d0b4d8e7a73275492a63ed7f39620b32b8
Ferdinand Verlandy <verlandyf@gmail.com>
2026-02-09 23:16:54 +0700

    week01: refactor to use val and string templates

---
+++ src/oop_00000123504_FerdinandVerlandy/week01/GradingSystem.kt
@@ -0,0 +1,12 @@
+package oop_00000123504_FerdinandVerlandy.week01
+
+fun main() {
+    val score = 85
+    val name = "Budi"
+
+    if (score >= 80) {
+        println("$name Lulus")
+    } else {
+        println("$name Tidak Lulus")
+    }
+}
