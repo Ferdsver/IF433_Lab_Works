c31c9bffeede3e0c8e856d75c6dcceb16372603a
Ferdinand Verlandy <verlandyf@gmail.com>
2026-02-09 23:28:27 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000123504_FerdinandVerlandy/week01/GameStore.kt
+++ src/oop_00000123504_FerdinandVerlandy/week01/GameStore.kt
@@ -3,4 +3,13 @@ package oop_00000123504_FerdinandVerlandy.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 750000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500_000) {
+        (price * 80) / 100
+    } else {
+        (price * 90) / 100
+    }
