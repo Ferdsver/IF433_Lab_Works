322cd525f2c7793b51a844c121eb3c533ae96421
Ferdinand Verlandy <verlandyf@gmail.com>
2026-02-09 23:18:09 +0700

    week01: implement grading logic using when expression

--- src/oop_00000123504_FerdinandVerlandy/week01/GradingSystem.kt
+++ src/oop_00000123504_FerdinandVerlandy/week01/GradingSystem.kt
@@ -4,9 +4,10 @@ fun main() {
     val score = 85
     val name = "Budi"
 
-    if (score >= 80) {
-        println("$name Lulus")
-    } else {
-        println("$name Tidak Lulus")
+    val status = when {
+        score >= 80 -> "Lulus"
+        else -> "Tidak Lulus"
     }
+
+    println("$name $status")
 }
