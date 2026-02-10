dcf61b17bde4fda1a7cf8466a1562f17f23642b1
Ferdinand Verlandy <verlandyf@gmail.com>
2026-02-11 00:00:58 +0700

    week02: add warning validation in init block

--- src/oop_00000123504_FerdinandVerlandy/week02/Student.kt
+++ src/oop_00000123504_FerdinandVerlandy/week02/Student.kt
@@ -4,4 +4,14 @@ class Student(
     val nim: String,
     val name: String,
     var major: String
-)
+) {
+    init {
+        if (nim.length != 10) {
+            println("WARNING: NIM tidak valid!")
+        }
+
+        if (name.isBlank()) {
+            println("WARNING: Nama kosong!")
+        }
+    }
+}
