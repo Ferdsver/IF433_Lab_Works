81cc5bd87ef2a9a38cc20c80f8419d74b0572ab9
Ferdinand Verlandy <verlandyf@gmail.com>
2026-03-09 17:27:28 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000123504_FerdinandVerlandy/week05/Main.kt
+++ src/oop_00000123504_FerdinandVerlandy/week05/Main.kt
@@ -2,6 +2,7 @@ package oop_00000123504_FerdinandVerlandy.week05
 
 fun main() {
 
+    // Polymorphic Collection Pegawai
     val pegawaiList: List<Pegawai> = listOf(
         Dosen("Budi"),
         Admin("Siti")
@@ -15,9 +16,27 @@ fun main() {
         }
     }
 
+    println("")
+
+    // Task 1 - MathHelper Overloading
     val mathHelper = MathHelper()
 
-    println(mathHelper.hitungLuas(4))
-    println(mathHelper.hitungLuas(5,3))
-    println(mathHelper.hitungLuas(7.0))
+    println("Luas Persegi: " + mathHelper.hitungLuas(4))
+    println("Luas Persegi Panjang: " + mathHelper.hitungLuas(5, 3))
+    println("Luas Lingkaran: " + mathHelper.hitungLuas(7.0))
+
+    println("")
+
+    // Task 2 - Payment Polymorphism
+    val ewallet = EWallet("Ferdinand", 50000.0)
+    val creditCard = CreditCard("Ferdinand", 100000.0)
+
+    val payments: List<PaymentMethod> = listOf(
+        ewallet,
+        creditCard
+    )
+
+    for (p in payments) {
+        p.processPayment(75000.0)
+    }
 }
