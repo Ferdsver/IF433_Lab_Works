b65d4b3f3d14b315a26b679c82a6139df36cc398
Ferdinand Verlandy <verlandyf@gmail.com>
2026-03-25 10:14:31 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000123504_FerdinandVerlandy/week07/Main.kt
+++ src/oop_00000123504_FerdinandVerlandy/week07/Main.kt
@@ -1,5 +1,8 @@
 package oop_00000123504_FerdinandVerlandy.week07
 
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id")
+    val client = NetworkClient.create("https://api.umn.ac.id")
+    println(client.baseUrl)
+
+    DatabaseManager.connect()
 }
