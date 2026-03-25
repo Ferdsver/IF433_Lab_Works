2daa6a89045723284cd451834f34501928063298
Ferdinand Verlandy <verlandyf@gmail.com>
2026-03-25 10:19:59 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000123504_FerdinandVerlandy/week07/SystemStates.kt
+++ src/oop_00000123504_FerdinandVerlandy/week07/SystemStates.kt
@@ -1,7 +1,7 @@
 package oop_00000123504_FerdinandVerlandy.week07
 
-enum class AppState {
-    LOADING,
-    SUCCESS,
-    ERROR
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
 }
