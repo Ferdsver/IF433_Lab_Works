4a702165dd9cde9b800ecaf96e5a94cfc7889159
Ferdinand Verlandy <verlandyf@gmail.com>
2026-04-29 11:02:30 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_00000123504_FerdinandVerlandy/week10/CryptoDashboard.kt
+++ src/oop_00000123504_FerdinandVerlandy/week10/CryptoDashboard.kt
@@ -9,4 +9,11 @@ fun main() {
     coinRepo.add(Coin("USDT", 5000.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
+
+    println("=== CRYPTO WALLET DASHBOARD ===")
+    println("Status: ${response.status}")
+
+    response.data.forEach {
+        println("Coin: ${it.name}, Balance: ${it.balance}")
+    }
 }
