2426875220435a02267455c1aac8232e61a11804
Ferdinand Verlandy <verlandyf@gmail.com>
2026-04-29 11:02:13 +0700

    week10: (task) populate coin repository

--- src/oop_00000123504_FerdinandVerlandy/week10/CryptoDashboard.kt
+++ src/oop_00000123504_FerdinandVerlandy/week10/CryptoDashboard.kt
@@ -3,4 +3,8 @@ package oop_00000123504_FerdinandVerlandy.week10
 fun main() {
 
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 5000.0))
 }
