550a0ecb82af15e57c8b6603d44d62ee9a83b988
Ferdinand Verlandy <verlandyf@gmail.com>
2026-05-21 17:43:46 +0700

    week13: implement basic writeText function

---
+++ src/oop_00000123504_FerdinandVerlandy/week11/ScopeLetRun.kt
@@ -0,0 +1,25 @@
+package oop_00000123504_FerdinandVerlandy.week11
+
+fun main() {
+
+    println("=== TEST LET FUNCTION ===")
+
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    } ?: 0
+
+    println("Panjang nama: $length")
+
+
+    println("\n=== TEST RUN FUNCTION ===")
+
+    val result = "Kotlin".run {
+        println("Memproses kata: $this")
+        length * 2
+    }
+
+    println("Hasil kalkulasi run: $result")
+}
