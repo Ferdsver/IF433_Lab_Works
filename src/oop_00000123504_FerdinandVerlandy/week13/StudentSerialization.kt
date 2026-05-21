7edf91681c20c316673a6da7da725dd20f2a8e04
Ferdinand Verlandy <verlandyf@gmail.com>
2026-05-21 17:45:20 +0700

    week13: create Student model

---
+++ src/oop_00000123504_FerdinandVerlandy/week13/StudentSerialization.kt
@@ -0,0 +1,10 @@
+package oop_00000123504_FerdinandVerlandy.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
