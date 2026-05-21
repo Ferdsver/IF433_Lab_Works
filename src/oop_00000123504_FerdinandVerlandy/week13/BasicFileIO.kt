a6e11ebf99e704978f07098e4d1d4db171905d73
Ferdinand Verlandy <verlandyf@gmail.com>
2026-05-21 17:43:58 +0700

    week13: implement appendText operation

--- src/oop_00000123504_FerdinandVerlandy/week13/BasicFileIO.kt
+++ src/oop_00000123504_FerdinandVerlandy/week13/BasicFileIO.kt
@@ -11,4 +11,9 @@ fun main() {
     file.writeText("Line 1: Inisialisasi sistem.\n")
 
     println("File berhasil dibuat dan ditulis.")
+
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+
+    println("Teks berhasil di-append.")
 }
