5294fbf0e5333efcf9949b2c1e404e8049875614
Ferdinand Verlandy <verlandyf@gmail.com>
2026-05-29 15:25:25 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000123504_FerdinandVerlandy/week14/EcommerceTask.kt
+++ src/oop_00000123504_FerdinandVerlandy/week14/EcommerceTask.kt
@@ -37,10 +37,7 @@ class CsvOrderRepository : OrderRepository {
     override fun saveOrder(data: String) {
 
         File("safe_orders.csv")
-            .printWriter()
-            .use { writer ->
-                writer.println(data)
-            }
+            .appendText("$data\n")
     }
 }
 
@@ -57,7 +54,71 @@ class EmailNotifier : NotificationService {
     }
 }
 
+
+interface PricingStrategy {
+
+    fun calculate(price: Double): Double
+}
+
+class VipPricing : PricingStrategy {
+
+    override fun calculate(price: Double): Double {
+        return price * 0.90
+    }
+}
+
+class RegularPricing : PricingStrategy {
+
+    override fun calculate(price: Double): Double {
+        return price
+    }
+}
+
+
 class SafeOrderProcessor(
     private val repo: OrderRepository,
     private val notifier: NotificationService
-)
+) {
+
+    fun processOrder(
+        itemName: String,
+        basePrice: Double,
+        strategy: PricingStrategy
+    ) {
+
+        val finalPrice = strategy.calculate(basePrice)
+
+        println("Processing order: $itemName -> $finalPrice")
+
+        repo.saveOrder("$itemName,$finalPrice")
+
+        notifier.sendNotification(
+            "Pesanan $itemName berhasil diproses!"
+        )
+    }
+}
+
+
+fun main() {
+
+    val repository = CsvOrderRepository()
+
+    val notifier = EmailNotifier()
+
+    val processor = SafeOrderProcessor(
+        repository,
+        notifier
+    )
+
+    processor.processOrder(
+        itemName = "Mechanical Keyboard",
+        basePrice = 1500000.0,
+        strategy = VipPricing()
+    )
+
+    processor.processOrder(
+        itemName = "Gaming Mouse",
+        basePrice = 500000.0,
+        strategy = RegularPricing()
+    )
+}
