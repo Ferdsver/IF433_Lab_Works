36f0cdf7e023dd7e325ed9e81f5db9d4b6542741
Ferdinand Verlandy <verlandyf@gmail.com>
2026-05-29 15:20:11 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000123504_FerdinandVerlandy/week14/OcpRefactored.kt
@@ -0,0 +1,20 @@
+package oop_00000123504_FerdinandVerlandy.week14
+
+interface DiscountStrategy {
+
+    fun apply(price: Double): Double
+}
+
+class StudentDiscount : DiscountStrategy {
+
+    override fun apply(price: Double): Double {
+        return price * 0.80
+    }
+}
+
+class MemberDiscount : DiscountStrategy {
+
+    override fun apply(price: Double): Double {
+        return price * 0.85
+    }
+}
